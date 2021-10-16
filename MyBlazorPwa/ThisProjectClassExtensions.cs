572ca2b75562580a7ba3102a576680702ceaa401
Rely Kao <rely_kao@asiavista.com.tw>
2021-10-17 00:59:13 +0800

    update

--- MyBlazorPwa/ThisProjectClassExtensions.cs
+++ MyBlazorPwa/ThisProjectClassExtensions.cs
@@ -20,5 +20,26 @@ namespace MyBlazorPwa
 
             return JsonSerializer.Serialize(self, options);
         }
+
+        /// <summary>
+        /// ref→https://stackoverflow.com/questions/58138793/system-text-json-jsonelement-toobject-workaround
+        /// </summary>
+        public static T ToObject<T>(this JsonElement element, JsonSerializerOptions options = null)
+        {
+            var bufferWriter = new System.Buffers.ArrayBufferWriter<byte>();
+            using (var writer = new Utf8JsonWriter(bufferWriter))
+                element.WriteTo(writer);
+            return JsonSerializer.Deserialize<T>(bufferWriter.WrittenSpan, options);
+        }
+
+        /// <summary>
+        /// ref→https://stackoverflow.com/questions/58138793/system-text-json-jsonelement-toobject-workaround
+        /// </summary>
+        public static T ToObject<T>(this JsonDocument document, JsonSerializerOptions options = null)
+        {
+            if (document == null)
+                throw new ArgumentNullException(nameof(document));
+            return document.RootElement.ToObject<T>(options);
+        }
     }
 }
