418fc3008740f40316bd1006fa8d854a048c3f7d
Rely Kao <rely_kao@asiavista.com.tw>
2021-10-16 22:15:16 +0800

    update

--- MyBlazorPwa/wwwroot/service-worker.js
+++ MyBlazorPwa/wwwroot/service-worker.js
@@ -2,3 +2,36 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 self.addEventListener('fetch', () => { });
+
+/// 處理文件訊息
+self.addEventListener('push', function (event) {
+  const payload = event.data ? event.data.text() : 'no payload';
+  const title = 'ServiceWorker Cookbook';
+  const options = {
+    body: payload,
+  };
+
+  // 送出 Notification
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+///// 處理物件訊息
+//self.addEventListener('push', event => {
+//  const textMsg = event.data.text(); // 若是純文字訊息用text()函式解開。
+//  const jsonObj = event.data.json(); // 若是Json物件封包用json()函式解開。
+//  console.log('Push Received.', { textMsg, jsonObj });
+//
+//  const { title, body, url, imageUrl } = jsonObj;
+//  const options = {
+//    icon: '/images/icon.png',
+//    badge: '/images/badge.png',
+//    body,
+//    image: imageUrl,
+//    data: {
+//      url
+//    }
+//  };
+//
+//  // 送出 Notification
+//  event.waitUntil(self.registration.showNotification(title, options));
+//});
---
+++ MyBlazorPwa/wwwroot/tools/swTools.js
@@ -0,0 +1,28 @@
+﻿
+
+/// return Object
+export function checkSupport() {
+  return {
+    serviceWorker: ('serviceWorker' in navigator),
+    PushManager: ('PushManager' in window),
+    Notification: ('Notification' in window),
+    indexedDB: ("indexedDB" in window)
+  }
+}
+
+
+/// ref→ https://github.com/GoogleChromeLabs/web-push-codelab/blob/master/app/scripts/main.js
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
